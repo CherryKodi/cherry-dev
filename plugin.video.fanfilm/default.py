0e6d49da77ff8b5a23f47104dba91f14aa8a1189
Robert Kalinowski <robert.kalinowski@sharkbits.com>
2018-06-30 21:51:33 +0200

    Move all libs from PTW to FanFilm

--- plugin.video.fanfilm/default.py
+++ plugin.video.fanfilm/default.py
@@ -18,7 +18,7 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
-import urlparse,sys,urllib
+import urlparse, sys, urllib
 from resources.lib.libraries import control
 import xbmcgui, xbmc
 
