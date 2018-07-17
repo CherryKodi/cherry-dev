fd6db3ec848bb80f6798539575513a01a26ca667
xulek <xulek121@gmail.com>
2018-07-17 13:28:33 +0200

    Aktualizacja skryptów

--- generator_zip.py
+++ generator_zip.py
@@ -25,6 +25,18 @@ class Generator:
                 ziph.write(os.path.join(root, file))
 
     def _generate_zip_file( self ):
+        if not os.path.exists("zip"):
+            os.makedirs("zip")
+        folder = "zip"
+        for the_file in os.listdir(folder):
+            file_path = os.path.join(folder, the_file)
+            try:
+                if os.path.isfile(file_path):
+                    os.unlink(file_path)
+                #elif os.path.isdir(file_path): shutil.rmtree(file_path)
+            except Exception as e:
+                print(e)
+                continue
         addons = os.listdir( "." )
         for addon in addons:
             try:
