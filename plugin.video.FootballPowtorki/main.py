25eb2d622fc64975634eea4e5d74d53a730cdf33
xulek <xulek121@gmail.com>
2018-07-19 21:45:23 +0200

    Narzędzie wspomagające budowanie wtyczek + ich użycie w FootballPowtorki

--- plugin.video.FootballPowtorki/main.py
+++ plugin.video.FootballPowtorki/main.py
@@ -1,139 +1,97 @@
 # -*- coding: UTF-8 -*-
 import urllib, urllib2, re, xbmc, xbmcplugin, xbmcgui, xbmc, xbmcaddon, HTMLParser, os
-import requests,json
+import requests, json, resolveurl , sys
+
+from ptw.libraries import addon_utils as addon
 from ptw.libraries import source_utils, dom_parser, client, cleantitle
-import resolveurl
-import sys
 
-reload(sys)
-sys.setdefaultencoding('utf8')
+from ptw.debug import log_exception, log, start_trace, stop_trace, TRACE_ALL
 
-pluginName=sys.argv[0].replace('plugin://','')
-basePath = "special://home/addons/"+pluginName+"resources/media/"
-resourcesPath = xbmc.translatePath(basePath)
-default_background = resourcesPath + "default.jpg"
+_pluginName = sys.argv[0].replace('plugin://','')
+_basePath = "special://home/addons/" + _pluginName + "resources/media/"
+_resourcesPath = xbmc.translatePath(_basePath)
+_default_background = _resourcesPath + "default.jpg"
+
 #=########################################################################################################=#
 #                                                   MENU                                                   #
 #=########################################################################################################=#
-def CATEGORIES():
-    #addDir("Szukaj", '', 1, '','','', True)
-    addDir("Premier Ligue", "http://www.replaymatches.com/search/label/search/label/Premier%20League", 2, resourcesPath + "pl.png",default_background,'', True)
-    addDir("La Liga", "http://www.replaymatches.com/search/label/La%20Liga", 2, resourcesPath + "laliga.png",default_background,'', True)
-    addDir("Serie A", "http://www.replaymatches.com/search/label/Serie%20A", 2, resourcesPath + "seriea.png",default_background,'', True)
-    addDir("Bundesliga", "http://www.replaymatches.com/search/label/Bundesliga", 2, resourcesPath + "bundesliga.png",default_background,'', True)
-    addDir("Ligue1", "http://www.replaymatches.com/search/label/Ligue%201", 2, resourcesPath + "ligue1.png",default_background,'', True)
-    addDir("Champions League", "http://www.replaymatches.com/search/label/UCL", 2, resourcesPath + "uefa.png",default_background,'', True)
-    addDir("Europa League", "http://www.replaymatches.com/search/label/UEL", 2, resourcesPath + "europa.png",default_background,'', True)
-    #addDir("FIFA", '', 9, '','','', True) TODO
 
-def SUBCATEGORIES(mode):
-    debug=1
-###################################################################################
+def CATEGORIES():
+    addon.addDir("Premier Ligue", "http://www.replaymatches.com/search/label/search/label/Premier%20League", mode='ListowanieMeczy', icon=_resourcesPath + "pl.png", fanart=_default_background)
+    addon.addDir("La Liga", "http://www.replaymatches.com/search/label/La%20Liga", mode='ListowanieMeczy', icon=_resourcesPath + "laliga.png", fanart=_default_background)
+    addon.addDir("Serie A", "http://www.replaymatches.com/search/label/Serie%20A", mode='ListowanieMeczy', icon=_resourcesPath + "seriea.png", fanart=_default_background)
+    addon.addDir("Bundesliga", "http://www.replaymatches.com/search/label/Bundesliga", mode='ListowanieMeczy', icon=_resourcesPath + "bundesliga.png", fanart=_default_background)
+    addon.addDir("Ligue1", "http://www.replaymatches.com/search/label/Ligue%201", mode='ListowanieMeczy', icon=_resourcesPath + "ligue1.png", fanart=_default_background)
+    addon.addDir("Champions League", "http://www.replaymatches.com/search/label/UCL", mode='ListowanieMeczy', icon=_resourcesPath + "uefa.png", fanart=_default_background)
+    addon.addDir("Europa League", "http://www.replaymatches.com/search/label/UEL", mode='ListowanieMeczy', icon=_resourcesPath + "europa.png", fanart=_default_background)
+
+############################################################################################################
 #=########################################################################################################=#
 #                                                 FUNCTIONS                                                #
 #=########################################################################################################=#
 
-    
-def addDir(name, url, mode, iconimage, thumb, opis, isFolder=True, total=1):
-    u=sys.argv[0]+'?url='+urllib.quote_plus(url)+'&mode='+str(mode)+'&name='+urllib.quote_plus(name)
-    ok = True
-    liz = xbmcgui.ListItem(name, iconimage, thumbnailImage=thumb)
-    url = thumb
-    liz.setArt({'thumb': iconimage,
-                'icon': iconimage,
-                'fanart': url})
-    liz.setInfo("Video", {'title':name , 'genre':'', 'rating': 0, 'plot': opis})
-    ok = xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]), url=u, listitem=liz, isFolder=isFolder, totalItems=total)
-    return ok
-
 def ListowanieMeczy():
-    #import pydevd
-    #pydevd.settrace(stdoutToServer=True, stderrToServer=True)
-    url = urllib.unquote_plus(params['url'])
-    result = client.request(url)
-    result_images = client.parseDOM(result, 'article', attrs={'class':'post hentry'})
-    result = client.parseDOM(result, 'h2', attrs={'class':'post-title entry-title'})
-    
-    images = client.parseDOM(result_images, 'a', ret='content')
-    links = client.parseDOM(result, 'a', ret='href')
-    titles = client.parseDOM(result, 'a', ret='title')
-    for item in zip(links,titles,images):
-        link = str(item[0])
-        title = str(item[1])
-        image = str(item[2])
-        addDir(title, link, 3, image,default_background,'', True)
+    try:
+        url = params['url']
+        result = client.request(url)
+        result_images = client.parseDOM(result, 'article', attrs={'class':'post hentry'})
+        result = client.parseDOM(result, 'h2', attrs={'class':'post-title entry-title'})
         
-def ListowanieLinkow():
-    url = urllib.unquote_plus(params['url'])
-    result = client.request(url)
-    nazwy = client.parseDOM(result, 'a', attrs={'class':'link-iframe'})
-    result = client.parseDOM(result, 'div', attrs={'dir':'ltr'})[2]
-    linki = client.parseDOM(result, 'a', ret='href')[1:len(nazwy)+1]
-    for item in zip(linki,nazwy):
-        link = str(item[0])
-        title = str(item[1])
-        addDir(title, link, 6, '',default_background,'', False)
+        images = client.parseDOM(result_images, 'a', ret='content')
+        links = client.parseDOM(result, 'a', ret='href')
+        titles = client.parseDOM(result, 'a', ret='title')
+        for item in zip(links,titles,images):
+            link = str(item[0])
+            title = str(item[1])
+            image = str(item[2])
+            addon.addDir(title, link, mode='ListowanieLinkow', icon=image, fanart=_default_background)
+    except:
+        log_exception()
 
-###################################################################################
+def ListowanieLinkow():
+    try:
+        url = params['url']
+        result = client.request(url)
+        nazwy = client.parseDOM(result, 'a', attrs={'class':'link-iframe'})
+        result = client.parseDOM(result, 'div', attrs={'dir':'ltr'})[2]
+        linki = client.parseDOM(result, 'a', ret='href')[1:len(nazwy)+1]
+        for item in zip(linki,nazwy):
+            link = str(item[0])
+            title = str(item[1])
+            addon.addLink(title, link, mode='OdpalanieLinku', fanart=_default_background)
+    except:
+        log_exception()
+
+def OdpalanieLinku():
+    url = params['url']
+    addon.PlayMedia(url)
+
+############################################################################################################
 #=########################################################################################################=#
 #                                               GET PARAMS                                                 #
-#=########################################################################################################=#    
-def get_params():
-    param = []
-    paramstring = sys.argv[2]
-    if len(paramstring) >= 2 :
-        params = sys.argv[2]
-        cleanedparams = params.replace('?', '')
-        if (params[len(params) - 1] == '/') :
-            params = params[0:len(params) - 2]
-        pairsofparams = cleanedparams.split('&')
-        param = {}
-        for i in range(len(pairsofparams)) :
-            splitparams = {}
-            splitparams = pairsofparams[i].split('=')
-            if (len(splitparams)) == 2:
-                param[splitparams[0]] = splitparams[1]                  
-    return param
+#=########################################################################################################=#
 
-params = get_params()
-url = None
-name = None
-mode = None
-iconimage = None
+params = addon.get_params()
+url = params.get('url')
+name = params.get('name')
+mode = params.get('mode')
+iconimage = params.get('iconimage')
 
-try:
-    url = urllib.unquote_plus(params['url'])
-except:
-    pass
-try:
-    name = urllib.unquote_plus(params['name'])
-except:
-    pass
-try:
-    mode = int(params['mode'])
-except:
-    pass
-try:
-    iconimage = urllib.unquote_plus(params['iconimage'])
-except:
-    pass
-        
-###################################################################################
-#=###########################################################################################################=#
-#                                                   MODES                                                     #
-#=###########################################################################################################=#
+############################################################################################################
+#=########################################################################################################=#
+#                                                   MODES                                                  #
+#=########################################################################################################=#
+
 if mode == None :
     CATEGORIES()
-elif mode == 1 :
-    mySearch()
-elif mode == 2 :
+elif mode == 'ListowanieMeczy' :
     ListowanieMeczy()
-elif mode == 3 :
+elif mode == 'ListowanieLinkow' :
     ListowanieLinkow()
-elif mode == 6 : 
-    url = urllib.unquote_plus(params['url'])
-    url = resolveurl.resolve(url)
-    xbmc.Player().play(str(url))
-    
-###################################################################################
-xbmcplugin.endOfDirectory(int(sys.argv[1]))
+elif mode == 'OdpalanieLinku' : 
+    OdpalanieLinku()
+
+############################################################################################################
+
+xbmcplugin.setContent(int(sys.argv[1]),'Movies')
+xbmcplugin.endOfDirectory(int(sys.argv[1]), cacheToDisc=True)
---
+++ script.module.ptw/lib/ptw/libraries/addon_utils.py
@@ -0,0 +1,63 @@
+# -*- coding: UTF-8 -*-
+import urllib, urllib2, xbmc, xbmcplugin, xbmcgui, xbmc, xbmcaddon
+import sys, resolveurl
+from ptw.debug import log_exception, log, start_trace, stop_trace, TRACE_ALL
+
+PY2 = sys.version_info[0] == 2
+if PY2:
+    from urlparse import parse_qs
+else:
+    from urllib.parse import parse_qs
+
+def addDir(name, url, mode='', icon='', thumb='', fanart='', poster='', banner='', clearart='', clearlogo='', genre='', year='', rating='', dateadded='', plot='', isFolder=True, total=1):
+    u=sys.argv[0]+'?url='+urllib.quote_plus(url)+'&mode='+str(mode)+'&name='+urllib.quote_plus(name)
+    liz = xbmcgui.ListItem(name)
+    liz.setArt({
+                'thumb': thumb,
+                'icon': icon,
+                'fanart': fanart,
+                'poster': poster,
+                'banner': banner,
+                'clearart': clearart,
+                'clearlogo': clearlogo
+                })
+    liz.setInfo("Video", {'title':name , 'genre': genre, 'year': year, 'rating': rating, 'dateadded': dateadded, 'plot': plot})
+    ok = xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]), url=u, listitem=liz, isFolder=isFolder, totalItems=total)
+    return ok
+
+def addLink(name, url, mode, icon='', thumb='', fanart='', poster='', banner='', clearart='', clearlogo='', genre='', year='', rating='', dateadded='', plot='', isFolder=False, total=1):
+    u=sys.argv[0]+'?url='+urllib.quote_plus(url)+'&mode='+str(mode)+'&name='+urllib.quote_plus(name)
+    ok = True
+    liz = xbmcgui.ListItem(name)
+    liz.setArt({
+                'thumb': thumb,
+                'icon': icon,
+                'fanart': fanart,
+                'poster': poster,
+                'banner': banner,
+                'clearart': clearart,
+                'clearlogo': clearlogo
+                })
+    liz.setInfo("Video", {'title':name , 'genre': genre, 'year': year, 'rating': rating, 'dateadded': dateadded, 'plot': plot})
+    ok = xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]), url=u, listitem=liz, isFolder=isFolder, totalItems=total)
+    return ok
+
+def get_params():
+    paramstring = sys.argv[2]
+    if paramstring.startswith('?'):
+        paramstring = paramstring[1:]
+    return dict((k, vv[0]) for k, vv in parse_qs(paramstring).items())
+
+def PlayMedia(link):
+    try:
+        pDialog = xbmcgui.DialogProgress()
+        pDialog.create('Odtwarzanie', 'Odpalanie linku...')
+        url = resolveurl.resolve(link)
+        if url is False:
+            raise ValueError('Nie udało się wyciągnąć linku')
+        pDialog.close()
+        xbmc.Player().play(str(url))
+    except Exception as e:
+        pDialog.close()
+        xbmcgui.Dialog().ok('Error', 'Błąd odpalania linku! %s' % e)
+        log_exception()
