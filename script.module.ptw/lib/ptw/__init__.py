c9e1d40239eed271f338769eb9f501a9f380bc1b
Robert Kalinowski <robert.kalinowski@sharkbits.com>
2018-07-26 23:17:54 +0200

    small fixes (PY2, .gitignore)

---
+++ script.module.ptw/lib/ptw/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: UTF-8 -*-
+# from __future__ import absolute_import, division, unicode_literals, print_function
+
+import sys
+
+PY2 = sys.version_info[0] == 2
+PY3 = sys.version_info[0] == 3
+
